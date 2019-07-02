fea3ecd3758246e2320e3b4ce69a4b104063b19d
marianohrl92 <52456039+marianohrl92@users.noreply.github.com>
2019-07-02 12:26:06 -0300

    Upload Solution build for SolsticeAPIChallenge

---
+++ Data/ContactContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SolsticeAPIChallenge.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolsticeAPIChallenge.Models
+{
+    public partial class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
+
