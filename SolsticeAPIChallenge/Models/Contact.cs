f0c127fdf52be7770e288b4ba9ff42ac86fef7ad
marianohrl92 <52456039+marianohrl92@users.noreply.github.com>
2019-07-02 13:20:20 -0300

    Reorganize folders

---
+++ SolsticeAPIChallenge/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolsticeAPIChallenge.Models
+{
+    public partial class Contact
+    {
+        public int ContactId { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Image { get; set; }
+        public string Email { get; set; }
+        public string Birthdate { get; set; }
+        public int PerPhoneNumber { get; set; }
+        public int WorkPhoneNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ SolsticeAPIChallenge/Models/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolsticeAPIChallenge.Models.Repository
+{
+    public interface IDataRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAllContacts();
+        TEntity GetContactById(long ContactId);
+        List<TEntity> GetAllContactsByStateOrAddress(string ContactId);
+        TEntity GetContactByEmailOrPhone(string ContactId);
+        TEntity PostNewContact(TEntity entity);
+        TEntity Update(TEntity dbEntity, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
